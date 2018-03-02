1fe85836e5f5525537082fc3a1bfe447ca6fbde4
Nebyoung <36654544+Nebyoung@users.noreply.github.com>
2018-03-02 00:41:20 -0600

    Frequency analysis done - sorting started
    sorting (desc) kinda done

--- test.js
+++ test.js
@@ -1,44 +1,81 @@
 function myPrint(evt)
 {	 
-	//if (window.File && window.FileReader && window.FileList && window.Blob)
-	//{
-		var f = evt[0]; 
-		var content = "";
-		alert(f);
-		if (f) {
-			var r = new FileReader();
-						alert("got here");
-
-			r.onload = function(e) { 
-				var contents = e.target.result;
-				content = contents;
-				alert( "Got the file\n" 
-					  +"name: " + f.name + "\n"
-					  +"type: " + f.type + "\n"
-					  +"size: " + f.size + " bytes\n"
-					  + "starts with: " + contents.substr(0, 50)
-				);  
-			}
-			alert("reading");
-			r.readAsText(f);
-		} else { 
-			alert("Failed to load file");
-			return;
+	//reading file
+	var f = evt[0]; 
+	alert(f);
+	if (f) {
+		var r = new FileReader();
+		r.onload = function(e) { 
+			var contents = e.target.result;
+			alert( "Got the file\n" 
+				  +"name: " + f.name + "\n"
+				  +"type: " + f.type + "\n"
+				  +"size: " + f.size + " bytes\n"
+				  + "starts with: " + contents.substr(0, 50)
+			);  
+			frequency(letters, contents);
 		}
-		alert("5");
-		
-		var letters = {};
-		for (int i = 0; i < 16; i++)
+		r.readAsText(f);
+	} else { 
+		alert("Failed to load file");
+		return;
+	}
+	//end read file
+	
+	//making list of machine code hex values - individual pairs for now
+	var hexChars = ["0", "1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+	var letters = [];
+	for (var i = 0; i < 16; i++)
+	{
+		for (var j = 0; j < 16; j++)
 		{
-			for (int j = 0; j < 16; j++)
-			{
-				
-			}
+			var index = i * 16 + j;
+			letters[index] = hexChars[i] + hexChars[j];
 		}
-		
-		
-
-	document.getElementById('demo').addEventListener('change', readSingleFile, false);
-		 
+	}
+	//end list of machine code hex values		 
 }
 
+function frequency(letters, text)
+{
+	//begin freq analysis
+	var array = text.split(" "); //all pairs are now parts of an array
+	var count = []; //frequency counter
+	for (var i = 0; i < letters.length; i++)
+	{
+		count[i] = 0; //initialize every count to 0;
+	}
+	var index = 0;
+	for (var i = 0; i < array.length; i++)
+	{
+		index = letters.indexOf(array[i]);
+		count[index]++;
+	}
+	//end freq analysis
+	
+	//sorting
+	var sortedCount = [];
+	var sortedChars = [];
+	for (var i = 0; i < count.length; i++)
+	{
+		var max = 0;
+		var temp = 0;
+		for (var j = 0; j < count.length; j++)
+		{
+			if (count[j] >= max)
+			{
+				max = count[j];
+				temp = j;
+			}
+		}
+		count[temp] = 0;
+		sortedCount[i] = max;
+		sortedChars[i] = letters[temp];
+	}
+	//end sorting
+	
+	for (var i = 0; i < sortedChars.length; i++)
+	{
+		alert(i + " : " + sortedChars[i] + ": " + sortedCount[i]); //print frequency of each hex
+	}
+}
