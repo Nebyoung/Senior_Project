158dde9a50d2af4130b578e365e75ac50029ae0b
Benjamin Young <36654544+Nebyoung@users.noreply.github.com>
2018-03-26 16:11:44 -0500

    Quicksort, filter for triple done - so fast!

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+
 function myPrint(evt)
 {	 
+
 	//reading file
 	var f = evt[0]; 
 	alert(f);
@@ -13,7 +15,10 @@ function myPrint(evt)
 				  +"size: " + f.size + " bytes\n"
 				  + "starts with: " + contents.substr(0, 50)
 			);  
-			tripleFrequency(letters, contents);
+			var array = tripleFrequency(letters, contents);
+				alert("calling");
+
+			saveFrequency(array);
 		}
 		r.readAsText(f);
 	} else { 
@@ -33,7 +38,56 @@ function myPrint(evt)
 			letters[index] = hexChars[i] + hexChars[j];
 		}
 	}
+	
 	//end list of machine code hex values		 
+}
+
+function saveFrequency(myArray)
+{
+	/*	alert("writing");
+
+	var fs = require('fs');
+
+	var file = fs.createWriteStream('array.txt');
+	file.on('error', function(err) { });
+	myArray.forEach(function(v) { file.write(v.join(', ') + '\n'); });
+	file.end();
+	alert("written");*/
+	/*alert("create file");
+	var blob= new Blob([content], {type: "text/plain;charset=utf-8"});
+	
+	saveAs(blob, frequency.text);
+	alert("created file");*/
+	var string = "";
+	for (var i = 0; i < myArray.length; i++)
+	{
+		string += "\n" + myArray[i];
+	}
+	document.getElementById("demo").innerHTML = string;
+
+	alert(string);
+	
+}
+
+function removeUnused(count, letters) 
+{
+	var newCount = [];
+	var newLetters = [];
+	var index = 0;
+	for (var i = 0; i < count.length; i++) 
+	{
+		if (count[i] <= 8) continue;
+		else 
+		{
+			newCount[index] = count[i];
+			newLetters[index] = letters[i];
+			index++;
+		}
+	}
+	var returnArr = [];
+	returnArr[0] = newCount;
+	returnArr[1] = newLetters;
+	return returnArr;
 }
 
 
@@ -62,11 +116,66 @@ function indexOf(letters, code)
 	return -1;
 }
 
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, letters, left, right) {
+
+    var pivot   = items[Math.round((-.5 + left + right) / 2)],
+        i       = left,
+        j       = right;
+
+
+    while (i <= j) {
+
+        while (items[i] > pivot) {
+            i++;
+        }
+
+        while (items[j] < pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(items, i, j);
+			swap(letters, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
+}
+
+function quickSort(items, letters, left, right) {
+
+    var index = 0;
+    if (items.length > 1) {
+        index = partition(items, letters, left, right);
+
+        if (left < index - 1) {
+            quickSort(items, letters, left, index - 1);
+        }
+
+        if (index < right) {
+            quickSort(items, letters, index, right);
+        }
+
+    }
+	var val = [];
+	val[0] = items;
+	val[1] = letters;
+    return val;
+}
 
 
 function singleFrequency(letters, text)
 {
 	//begin freq analysis
+	var finalArray = [];
 	var array = text.split(" "); //all pairs are now parts of an array
 	var count = []; //frequency counter
 	for (var i = 0; i < letters.length; i++)
@@ -102,11 +211,17 @@ function singleFrequency(letters, text)
 		sortedChars[i] = letters[temp];
 	}
 	//end sorting
-	
-	for (var i = 0; i < sortedChars.length; i++)
+
+	index = 0;
+	for (var i = 0; i < sortedCount.length; i++)
 	{
-		alert(i + " : " + sortedChars[i] + ": " + sortedCount[i]); //print frequency of each hex
+		if (sortedCount[i] <= 5) continue;
+		//alert(i + " : " + tripleLetters[i] + ": " + count[i]); //print frequency of each hex
+		var temp = (i + " : " + sortedChars[i] + ": " + sortedCount[i]);
+		finalArray[index] = temp;
+		index++;
 	}
+	return finalArray;
 }
 
 function doubleFrequency(letters, text)
@@ -114,6 +229,7 @@ function doubleFrequency(letters, text)
 	//begin doublefreq analysis
 	var array = text.split(" ");
 	var count = [];
+	var finalArray = [];
 	var length = letters.length * letters.length;
 	var doubleLetters = [];
 	for (var i = 0; i < letters.length; i++)
@@ -133,20 +249,50 @@ function doubleFrequency(letters, text)
 		index = indexOf(doubleLetters, (array[i] + array[i + 1]));
 		count[index]++;
 	}
-	alert("done counting");
+	/*alert("done counting");
 	for (var i = 0; i < count.length - 1; i++)
 	{
 		if (count[i] == 0) continue;
 		alert(i + " : " + doubleLetters[i] + ": " + count[i]); //print frequency of each hex
+	}*/
+	var sortedCount = [];
+	var sortedChars = [];
+	for (var i = 0; i < count.length; i++)
+	{
+		var max = 0;
+		var temp = 0;
+		for (var j = 0; j < count.length; j++)
+		{
+			if (count[j] >= max)
+			{
+				max = count[j];
+				temp = j;
+			}
+		}
+		count[temp] = 0;
+		sortedCount[i] = max;
+		sortedChars[i] = doubleLetters[temp];
 	}
+	
+	index = 0;
+	for (var i = 0; i < sortedCount.length; i++)
+	{
+		if (sortedCount[i] <= 5) continue;
+		//alert(i + " : " + tripleLetters[i] + ": " + count[i]); //print frequency of each hex
+		var temp = (i + " : " + sortedChars[i] + ": " + sortedCount[i]);
+		finalArray[index] = temp;
+		index++;
+	}
+	return finalArray;
 }
 
 
 function tripleFrequency(letters, text)
 {
-	//begin doublefreq analysis
+	//begin triplefreq analysis
 
 	var array = text.split(" ");
+	var finalArray = [];
 	var count = [];
 	var lengthCubed = letters.length * letters.length * letters.length;
 	var lengthSquared = letters.length * letters.length;
@@ -176,24 +322,33 @@ function tripleFrequency(letters, text)
 				}
 			}
 		}
-		alert("starting to count");
 		var index = 0;
 		for (var i = 0; i < array.length - 2; i++)
 		{
-			//index = tripleLetters.indexOf(array[i] + array[i + 1] + array[i + 2]);
-			//if (i % 10000 == 0) alert(i + " " + (array[i] + array[i + 1] + array[i + 2]));
 			index = indexOf(tripleLetters, (array[i] + array[i + 1] + array[i + 2]));
 			count[index]++;
 		}
-		alert("done counting");
-		for (var i = 0; i < count.length; i++)
+		var trimmedArr = removeUnused(count, tripleLetters);
+		
+		alert("trimmed length is " + trimmedArr[0].length);
+		
+		var tmp = quickSort(trimmedArr[0], trimmedArr[1], 0, trimmedArr[0].length - 1);
+		var sortedCount = tmp[0];
+		var sortedChars = tmp[1];
+		
+		index = 0;
+		for (var i = 0; i < sortedCount.length; i++)
 		{
-			if (count[i] == 0) continue;
-			alert(i + " : " + tripleLetters[i] + ": " + count[i]); //print frequency of each hex
+			var temp = (i + " : " + sortedChars[i] + ": " + sortedCount[i]);
+			finalArray[index] = temp;
+			index++;
 		}
+		//end sorting
+			
+		return finalArray;
 	}
 	catch (ex)
 	{
-		alert("I DIED");
+		alert(ex.message);
 	}
 }
